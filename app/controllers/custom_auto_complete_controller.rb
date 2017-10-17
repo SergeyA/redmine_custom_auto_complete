3192d93229955f3e252a192b5cae10cd4f94451a
SergeyA <sergey.alexeev@gmail.com>
2017-10-17 13:22:04 +0300

    'per project' based values list

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -4,7 +4,7 @@ class CustomAutoCompleteController < ApplicationController
   before_filter :find_project, :authorize
 
   def search
-    @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:custom_field_id], "%#{params[:term]}%"])
+    @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
     render :layout => false
   end
 
