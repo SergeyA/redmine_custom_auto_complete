305171dc2159cae95349aa056b23ff06a963d760
SergeyA <sergey.alexeev@gmail.com>
2017-10-18 16:57:35 +0300

    Update custom_auto_complete_controller.rb

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -4,8 +4,19 @@ class CustomAutoCompleteController < ApplicationController
   before_filter :find_project, :authorize
 
   def search
-    @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
-    render :layout => false
+    begin  
+      @page = Wiki.find_page("Builds", project => params[:project_id])
+
+      @issues = @page.text.split(/[\r\n]+/)
+        .each_with_index
+        .map {|x, i| { 'id' => i, 'value' => x.split(/\s/).first } }
+    
+      # @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
+      render :layout => false
+
+    rescue ActiveRecord::RecordNotFound
+      render_404
+    end       
   end
 
 private
