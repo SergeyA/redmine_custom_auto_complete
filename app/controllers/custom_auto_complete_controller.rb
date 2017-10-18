82434df1f853a48c4ca399d05bb39fdd98525f63
SergeyA <sergey.alexeev@gmail.com>
2017-10-18 17:27:47 +0300

    Update custom_auto_complete_controller.rb

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -6,9 +6,14 @@ class CustomAutoCompleteController < ApplicationController
   def search
     # @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
 
+    @pattern = {params[:term]}.to_s.strip.downcase;
+    
     @issues = @page.text.split(/[\r\n]+/)
+      .select { |x| !x.blank }
+      .select { |x| @pattern.blank || x.downcase.include? @pattern }
+      .reverse
       .each_with_index
-      .map {|x, i| OpenStruct.new({id: i, value: x.split(/\s/).first}) }
+      .map { |x, i| OpenStruct.new({id: i, value: x.split(/\s/).first}) }
 
     render :layout => false
   end
