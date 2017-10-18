58a984dde15d503c13c4d2523cbda6a897220a02
SergeyA <sergey.alexeev@gmail.com>
2017-10-18 17:07:08 +0300

    Update custom_auto_complete_controller.rb

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -1,27 +1,27 @@
 class CustomAutoCompleteController < ApplicationController
   unloadable
 
-  before_filter :find_project, :authorize
+  before_filter :find_project, :find_wikipage, :authorize
 
   def search
-    begin  
-      @page = Wiki.find_page("Builds", :project => params[:project_id])
+    # @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
 
-      @issues = @page.text.split(/[\r\n]+/)
-        .each_with_index
-        .map {|x, i| { 'id' => i, 'value' => x.split(/\s/).first } }
-    
-      # @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
-      render :layout => false
+    @issues = @page.text.split(/[\r\n]+/)
+      .each_with_index
+      .map {|x, i| { 'id' => i, 'value' => x.split(/\s/).first } }
 
-    rescue ActiveRecord::RecordNotFound
-      render_404
-    end       
+    render :layout => false
   end
 
 private
   def find_project
     @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end 
+
+  def find_wikipage
+    @page = Wiki.find_page("Builds", :project => @project)
   rescue ActiveRecord::RecordNotFound
     render_404
   end 
