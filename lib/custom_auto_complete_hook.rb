e0fdb51f13ea17adaf3c90b4aac84206a75d5fc0
SergeyA <sergey.alexeev@gmail.com>
2017-10-19 16:13:50 +0300

    Update custom_auto_complete_hook.rb

--- lib/custom_auto_complete_hook.rb
+++ lib/custom_auto_complete_hook.rb
@@ -1,7 +1,7 @@
 class CustomAutoCompleteHookListener < Redmine::Hook::ViewListener
 
   def view_layouts_base_html_head(context)
-    css = Setting.redmine_custom_auto_complete['css']
+    css = Setting.plugin_redmine_custom_auto_complete['css']
     "<style type=\"text/css\">#{css}</style>" unless css.nil? or css.empty?
   end
   
