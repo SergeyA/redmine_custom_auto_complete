a48105815fc7ed91dcc15c9654f2885f0d2cb433
SergeyA <sergey.alexeev@gmail.com>
2017-10-19 18:11:29 +0300

    Update custom_auto_complete_controller.rb

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -20,7 +20,7 @@ class CustomAutoCompleteController < ApplicationController
     if source.to_s.strip.empty? then
       @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])            
     else
-      logger.info("pageName" + source.to_s.strip)
+      logger.error("pageName" + source.to_s.strip)
       
       page = find_wikipage(source.to_s.strip)
       
