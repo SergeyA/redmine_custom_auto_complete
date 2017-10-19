fa4e46bba3a98f8c97c8cdfc3a94834e1dd0e0f8
SergeyA <sergey.alexeev@gmail.com>
2017-10-19 18:02:16 +0300

    Update custom_auto_complete_controller.rb

--- app/controllers/custom_auto_complete_controller.rb
+++ app/controllers/custom_auto_complete_controller.rb
@@ -4,19 +4,41 @@ class CustomAutoCompleteController < ApplicationController
   before_filter :find_project, :find_wikipage, :authorize
 
   def search
-    # @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])
 
-    @pattern = params[:term].to_s.strip.downcase;
+    source = nil
+    values = nil
     
-    @issues = @page.text.split(/[\r\n]+/)
-      .map { |x| x.to_s.strip.split(/\s/).first }
-      .select { |x| !x.empty? }
-      .select { |x| "1".empty? || x.downcase.include?("1") }
-      .uniq
-      .reverse
-      .each_with_index
-      .map { |x, i| OpenStruct.new({ id: i, value: x }) }
+    5.times do |idx|
+      field = Setting.plugin_redmine_custom_auto_complete['acfield' + idx.to_s]
+      if !field.nil? && Regexp.new(field, true).match(@field.name) then
+        source = Setting.plugin_redmine_custom_auto_complete['acsource' + idx.to_s]
+        values = Setting.plugin_redmine_custom_auto_complete['acvalues' + idx.to_s]
+        break
+      end
+    end
     
+    if source.to_s.strip.empty? then
+      @issues = Issue.find_by_sql(["select max(issues.id) as id, custom_values.value as value, count(*) as count from custom_values, issues where issues.project_id = ? and custom_values.customized_id = issues.id and custom_values.custom_field_id = ? and lower(custom_values.value) like lower(?) group by custom_values.value order by max(issues.updated_on) desc", params[:project_id], params[:custom_field_id], "%#{params[:term]}%"])            
+    else
+      page = find_wikipage(source)
+      
+      if page.nil? then
+        render_404
+      else
+        pattern = params[:term].to_s.strip.downcase
+        
+        @issues = page.text.split(/[\r\n]+/)
+          .map { |x| x.to_s.strip.split(/\s/).first }
+          .concat values.to_s.split(/\;/)
+          .select { |x| !x.empty? }
+          .select { |x| pattern.empty? || x.downcase.include?(pattern) }
+          .uniq
+          .reverse
+          .each_with_index
+          .map { |x, i| OpenStruct.new({ id: i, value: x }) }     
+      end
+    end
+
     render :layout => false
   end
 
@@ -27,10 +49,16 @@ private
     render_404
   end 
 
-  def find_wikipage
-    @page = Wiki.find_page("Builds", :project => @project)
+  def find_field
+    @field = CustomField.find(params[:custom_field_id])
   rescue ActiveRecord::RecordNotFound
     render_404
+  end 
+
+  def find_wikipage(pageName)
+    return Wiki.find_page(pageName, :project => @project)
+  rescue ActiveRecord::RecordNotFound
+    return nil
   end 
 
 end
