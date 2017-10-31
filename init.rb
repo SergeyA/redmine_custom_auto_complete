8bf1707b8b1ffc8de8e48248672ecffbb51c1a51
SergeyA <sergey.alexeev@gmail.com>
2017-10-31 09:32:09 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ Redmine::Plugin.register :redmine_custom_auto_complete do
   require 'custom_auto_complete_hook'
 
   name 'Redmine Custom Auto-Complete plugin'
-  author 'Taiki'
+  author 'Sergey Alexeev, Taiki'
   description 'This plugin adds auto-complete to the custom fields'
   version '0.0.2'
-  url 'https://github.com/taikii/redmine_custom_auto_complete'
-  author_url 'https://github.com/taikii'
+  url 'https://github.com/SergeyA/redmine_custom_auto_complete'
+  author_url 'https://github.com/SergeyA'
 
   settings :default => { :css => '' }, :partial => 'settings/custom_auto_complete'
   
